e56f3d2bf747e623b58ee4e2c4613b22c06a6446
Rick Mills <rick@evoluted.net>
2016-10-24 10:39:29 +0100

    Added direct link complete purchase request class

---
+++ src/Omnipay/BarclaysEpdq/Message/DirectLinkCompletePurchaseRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Omnipay\BarclaysEpdq\Message;
+
+use Omnipay\BarclaysEpdq\Message\EssentialCompletePurchaseRequest;
+
+/**
+ * BarclaysEpdq Complete Purchase Request
+ */
+class DirectLinkCompletePurchaseRequest extends EssentialCompletePurchaseRequest
+{
+
+}
+
